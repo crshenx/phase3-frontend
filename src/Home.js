84860d9e2048c7a194b25b92ec8e950861ac5c6c
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-08-01 16:57:52 -0600

    landing page

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="splash">
+            <div className="homeContainer">
+                <h1>Wedding Dress Shop</h1>
+                <section className="description">
+                    <p>If you have ever tried to shop for a wedding dress, you'll understand why this app is essential.</p>
+                    <p>In the world of endless options, saying "yes to the dress" is as easy as clicking a button. Your wedding dress is at the tips of your fingers!</p>
+                    <button>Say Yes to the dress!</button> 
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default Home
