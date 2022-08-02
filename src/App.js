f48e0aa22350f938bbc674ab6fe41b678606efd5
Chris Hendricks <crshenx@gmail.com>
2022-08-02 08:40:46 -0700

    throw away

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import { useEffect, useState } from "react";
-<<<<<<< HEAD
-import Home from "./components/home";
-=======
 import NavBar from "./NavBar.js";
 import Home from "./Home.js";
-import './App.css';
-
->>>>>>> 5ae0fff7064d00663369dc2921db84779192db21
+import "./App.css";
 
 function App() {
   const [render, setRender] = useState([]);
@@ -19,16 +14,12 @@ function App() {
 
   console.log(render);
 
-<<<<<<< HEAD
-  return <Home />;
-=======
   return (
     <div>
       <NavBar />
-      <Home/>
+      <Home />
     </div>
-  )
->>>>>>> 5ae0fff7064d00663369dc2921db84779192db21
+  );
 }
 
 export default App;
