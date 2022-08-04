90652d683f32e6d76598967138e39f8274b795fb
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-08-04 10:28:23 -0600

    morning changes

--- src/Components/Catalog.js
+++ src/Components/Catalog.js
@@ -6,28 +6,32 @@ import { Container } from "@mui/material";
 
 function Catalog({ render, handleCartRender }) {
   return (
-    <Container>
+    <div>
       <SearchBar render={render} />
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        rowSpacing={2}
-        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-      >
+    <Container>
+    <Grid
+    container
+    spacing={4}
+    direction="row"
+    justifyContent="center"
+    alignItems="center"
+    rowSpacing={2}
+    columnSpacing={{ xs: 1, sm: 10, md: 3 }}
+    >
         {render.map((dress) => {
           return (
+            <Grid item>
             <DressCard
               dress={dress}
               key={dress.id}
               handleCartRender={handleCartRender}
             />
+            </Grid>
           );
         })}
       </Grid>
-
     </Container>
+    </div>
   );
 }
 export default Catalog;
--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,12 @@ function Home() {
     return (
         <div className="splash">
             <div className="homeContainer">
-                <h1>Yes to The Dress!</h1>
+                <img className="store-name" src="https://i.imgur.com/yEvjfK4.png" alt="store-name"/>
                 <section className="description">
                     <p>If you have ever tried to shop for a wedding dress, you'll understand why this app is essential.</p>
                     <p>In the world of endless options, saying "yes to the dress" is as easy as clicking a button. Your wedding dress is at the tips of your fingers!</p>
                     <Link to={`/Catalog`}>
-                    <button className='button btn button-primary'>Say yes to the dress →</button> 
+                    <button className='button btn button-primary'>Say yes! →</button> 
                     </Link>
                 </section>
             </div>
