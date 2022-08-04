de3d90811b2fa3183123257a0e460ebc0385eea4
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-08-04 17:08:44 -0600

    changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,17 +1,38 @@
 import React from 'react'
+// import { Grid } from "@mui/material";
 
 function About() {
   return (
+    // <Grid container spacing={2} columns={16}>
     <div className='about'>
         <div className='aboutContainer'>
-            <h1>About the Creators</h1>
+            <h1 className='about-title'>About the Creators</h1>
+            {/* <Grid item> */}
             <img className="lauren-pic" src='https://i.imgur.com/Y9lz9It.png' alt="Lauren pic" />
             <section className='about-description'>
-                <p>Hello, my name is Lauren and this is our project for phase 3 at Flatiron school using Ruby for our database and React.js for our frontend. I am pursuing a career change from teaching to tech with the goal to become a full-stack software engineer. I want to help modernize and develop educational technology that gives educators more resources for equitable education.</p><br></br>
-            <img className='chris-pic' src=" " alt="Chris Pic" />
-            <p>Chris about here</p>
+                <p>Hello, my name is Lauren and this is our project for phase 3 at Flatiron school using Ruby for our database and React.js for our frontend. I am pursuing a career change from teaching to tech with the goal to become a full-stack software engineer. I want to help modernize and develop educational technology that gives educators more resources for equitable education.</p>
+                <a href="www.linkedin.com/in/lauren-erxleben">linkedin</a> 
+                <br></br>
+                <a href="https://github.com/laurennoelle">Github</a>
+                <br></br>
+                <br></br>
+            {/* <img className='chris-pic' src=" " alt="Chris Pic" />
+            <p>Chris about here</p> */}
             </section>
-
+            {/* </Grid>
+            <Grid item> */}
+            <img className="lauren-pic" src='https://i.imgur.com/Y9lz9It.png' alt="Lauren pic" />
+            <section className='about-description'>
+                <p>Hello, my name is Lauren and this is our project for phase 3 at Flatiron school using Ruby for our database and React.js for our frontend. I am pursuing a career change from teaching to tech with the goal to become a full-stack software engineer. I want to help modernize and develop educational technology that gives educators more resources for equitable education.</p>
+                <a href="www.linkedin.com/in/lauren-erxleben">linkedin</a> 
+                <br></br>
+                <a href="https://github.com/laurennoelle">Github</a>
+                <br></br>
+                <br></br>
+            {/* <img className='chris-pic' src=" " alt="Chris Pic" />
+            <p>Chris about here</p> */}
+            </section>
+            {/* </Grid> */}
         </div>
 
 
@@ -21,6 +42,7 @@ function About() {
 
 
     </div>
+    // </Grid>
   )
 }
 
