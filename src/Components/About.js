91bb1569d16447b11e69747417ab01a7877c8a15
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-08-04 19:21:30 -0600

    about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,32 +7,33 @@ function About() {
     <div className='about'>
         <div className='aboutContainer'>
             <h1 className='about-title'>About the Creators</h1>
-            {/* <Grid item> */}
+            <section className='indent-1'>
+            <section className='about-description-1'>
             <img className="lauren-pic" src='https://i.imgur.com/Y9lz9It.png' alt="Lauren pic" />
-            <section className='about-description'>
                 <p>Hello, my name is Lauren and this is our project for phase 3 at Flatiron school using Ruby for our database and React.js for our frontend. I am pursuing a career change from teaching to tech with the goal to become a full-stack software engineer. I want to help modernize and develop educational technology that gives educators more resources for equitable education.</p>
-                <a href="www.linkedin.com/in/lauren-erxleben">linkedin</a> 
+                <a className="linkedin-icon" href="https://linkedin.com/in/lauren-erxleben" target="_blank">
+                <ion-icon name="logo-linkedin"></ion-icon></a>
+                <a className="github-icon" href="https://github.com/laurennoelle" target="_blank">
+                <ion-icon name="logo-github"></ion-icon></a>
+              
                 <br></br>
-                <a href="https://github.com/laurennoelle">Github</a>
                 <br></br>
                 <br></br>
-            {/* <img className='chris-pic' src=" " alt="Chris Pic" />
-            <p>Chris about here</p> */}
+
             </section>
-            {/* </Grid>
-            <Grid item> */}
+           
+            <section className='about-description-2'>
             <img className="lauren-pic" src='https://i.imgur.com/Y9lz9It.png' alt="Lauren pic" />
-            <section className='about-description'>
                 <p>Hello, my name is Lauren and this is our project for phase 3 at Flatiron school using Ruby for our database and React.js for our frontend. I am pursuing a career change from teaching to tech with the goal to become a full-stack software engineer. I want to help modernize and develop educational technology that gives educators more resources for equitable education.</p>
-                <a href="www.linkedin.com/in/lauren-erxleben">linkedin</a> 
+                <a className="linkedin-icon" href="" target="_blank">
+                <ion-icon name="logo-linkedin"></ion-icon></a>
+                <a className="github-icon" href="" target="_blank">
+                <ion-icon name="logo-github"></ion-icon></a>
                 <br></br>
-                <a href="https://github.com/laurennoelle">Github</a>
                 <br></br>
-                <br></br>
-            {/* <img className='chris-pic' src=" " alt="Chris Pic" />
-            <p>Chris about here</p> */}
             </section>
-            {/* </Grid> */}
+            </section>
+           
         </div>
 
 
